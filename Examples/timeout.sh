9749f388ee68d0022edcff79e7cacd5d45c0d957
gengyazhao <1716133985@qq.com>
2023-08-26 16:35:56 +0800

    Update timeout.sh

--- Examples/timeout.sh
+++ Examples/timeout.sh
@@ -27,3 +27,23 @@ echo
 #这个脚本的行为可能与timed-input.sh不同，在每次按键的时候，计数器都会重置。
 
 exit 0
+------------------------------------------------------------------------------
+
+#!/bin/bash
+#time-out.sh
+#
+TIMELIMIT=4
+
+read -t $TIMELIMIT variable <&1
+#在这个例子中，对于bash 1.x和2.x就需要使用“<&1”
+#但对于bash 3.x就不需要
+
+echo
+#Is null?
+if [ -z "$variable" ];then
+        echo "Timed out, variable still unset."
+else
+        echo "variable = $variable"
+fi
+
+exit 0
